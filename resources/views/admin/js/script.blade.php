94c9593f1efa938d5a16c10336566ca7ceedcc80
Alawi8 <38544867+Alawi8@users.noreply.github.com>
2022-10-12 16:19:21 +0300

    edit here

--- resources/views/admin/js/script.blade.php
+++ resources/views/admin/js/script.blade.php
@@ -1,8 +1,9 @@
 <script>
     /*!
-    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2022 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    It is required to add some improvements to the 
+    admin/index
+    all admin/pages 
+     as the developer deems appropriate.
     */
     /
 // Scripts
