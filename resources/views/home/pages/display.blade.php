ee72e16d1315c94e9e4eb85f58196dd79bce4a29
Alawi8 <38544867+Alawi8@users.noreply.github.com>
2022-11-03 00:54:19 +0300

    update some change in home

--- resources/views/home/pages/display.blade.php
+++ resources/views/home/pages/display.blade.php
@@ -5,8 +5,8 @@
     <div class="container-fluid">
         <h1>{{ $dis_posts->title }}</h1>
         <img id="card-content-posts" class="card-img-top rounded-3 text-center"
-            src="{{ asset('public/img/offers/' . $dis_posts->image_path) }}"
-            name="{{ asset('storage/img/' . $dis_posts->image_path) }}">
+            {{-- src="{{ asset('public/img/offers/' . $dis_posts->image_path) }}" --}}
+            {{-- name="{{ asset('storage/img/' . $dis_posts->image_path) }}"> --}}
         <br>
         <div  id="content">
             @php
