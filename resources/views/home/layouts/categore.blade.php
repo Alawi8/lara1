0c5def0a7ea418feaef301c821dc5609a8dcd7c6
Alawi8 <38544867+Alawi8@users.noreply.github.com>
2022-11-03 15:52:22 +0300

    upgrade home

--- resources/views/home/layouts/categore.blade.php
+++ resources/views/home/layouts/categore.blade.php
@@ -4,24 +4,24 @@
         <input class="form-control me-2 " type="search" placeholder="بحث" aria-label="Search">
         <button class="btn btn-outline-light bg-success" type="submit">البحث</button>
     </form><br>
-    <div class="card">
+    <div id="categure" class="card ">
         <div class="card-footer">التصنيفات
         </div>
-        <ul class="list-group list-group-flush">
+        <ul class="list-group ">
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link " href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
         </ul>
@@ -30,19 +30,19 @@
         </div>
         <ul class="list-group list-group-flush">
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
         </ul>
         <div class="card-footer">
@@ -50,19 +50,19 @@
         </div>
         <ul class="list-group list-group-flush">
             <li class="nav-item">
-                <a class="nav-link px-2 text-muted" href="#">link</a>
+                <a class="nav-link btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
         </ul>
         <div class="card-footer">
@@ -70,19 +70,19 @@
         </div>
         <ul class="list-group list-group-flush">
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
         </ul>
         <div class="card-footer">
@@ -90,19 +90,19 @@
         </div>
         <ul class="list-group list-group-flush">
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
 
             <li class="nav-item">
-                <a class="text-center nav-link" href="#">link</a>
+                <a class="text-center btn btn-light nav-link" href="#">link</a>
             </li>
         </ul>
     </div>
