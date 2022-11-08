02217835599dbf1200fab77e1149ded6864c554f
Alawi8 <alialshalawi9@gmail.com>
2022-11-08 10:08:22 +0300

    fix problem new

--- resources/views/home/layouts/footer.blade.php
+++ resources/views/home/layouts/footer.blade.php
@@ -4,9 +4,9 @@
     <div  class="container ">
         <footer  class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             {{-- style bootstrap --}}
-            <link rel="stylesheet" href="{{ asset('/public/css/bootstrap.css') }}">
+            <link rel="stylesheet" href="{{ asset('/public/css/bootstrapp.min.css') }}">
             {{-- link style.css to disegn header tamplate --}}
-            <link rel="stylesheet" href="{{ URL('/public/css/style.css') }}">
+            <link rel="stylesheet" href="{{ asset ('/public/css/style.min.css') }}">
             {{-- link bootstrap.js to action  --}}
             <script src="{{ asset('/public/js/bootstrap.js') }}"></script>
             
--- resources/views/home/welcom.blade.php
+++ resources/views/home/welcom.blade.php
@@ -1,6 +1,4 @@
-<div class="bg-light">
     @extends('home.layouts.body')
     @section('content')
         @include('home.pages.home')
     @endsection
-</div>
