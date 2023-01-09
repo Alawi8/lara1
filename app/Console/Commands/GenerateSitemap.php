d5df23459359b1a3edbb4378927f4df0b16d5364
Alawi8 <alialshalawi9@gmail.com>
2023-01-09 09:55:31 +0300

    update seo

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -84,7 +84,7 @@ class GenerateSitemap extends Command
             if (isset($url)) {
                 $result = $dom->createElement('url');
                 $root->appendChild($result);
-                $result->appendChild($dom->createElement('loc', "https://laravel.test" . $url));
+                $result->appendChild($dom->createElement('loc', "https://localhost" . $url));
                 $result->appendChild($dom->createElement('priority', '1.0'));
                 $result->appendChild($dom->createElement('lastmod', $time->format(\DateTime::ATOM)));
                 $dom->save(public_path('/sitemap/' . $fileName . '.xml'));
