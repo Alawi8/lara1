03e08db8757374404086285c0e89bd49da71adf0
Alawi8 <alialshalawi9@gmail.com>
2023-01-31 17:05:29 +0300

    update error https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,13 +30,6 @@ class AppServiceProvider extends ServiceProvider
     
     public function isSecure()
     {
-        $isSecure = false;
-        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-            $isSecure = true;
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
-            $isSecure = true;
-        }
-    
-        return $isSecure;
+        // 
     }
 }
