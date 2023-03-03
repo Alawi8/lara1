ea878b98008a715c2456e67ba1410893a45a8efb
Alawi8 <alialshalawi9@gmail.com>
2023-03-03 10:51:22 +0300

    update css

--- resources/views/home/pages/display.blade.php
+++ resources/views/home/pages/display.blade.php
@@ -5,12 +5,14 @@
 
 @section('content')
     {{-- {{$dis_posts}} --}}
-    <div id="content-items" class="container-fluid ">
+    <div id="content-items" class="container-fluid  ">
         <h1>{{ $dis_posts->title }}</h1>
         <div id="content-img-posts-card" class="rounded-4 text-center">
             <img  
                 src="{{$dis_posts->image_path }}" name="{{ $dis_posts->title }}"
-                alt="{{ $dis_posts->title }}">
+                alt="{{ $dis_posts->title }}"
+                class="legend img-fluid">
+                
         </div>
         <br>
         <div id="content" name="">
