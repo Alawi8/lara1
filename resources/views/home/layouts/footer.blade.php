fa892983601cee52660c27f4e23cba1a216ae79d
Alawi8 <alialshalawi9@gmail.com>
2023-12-04 16:53:42 +0300

    edit bootstrap url

--- resources/views/home/layouts/footer.blade.php
+++ resources/views/home/layouts/footer.blade.php
@@ -3,17 +3,17 @@
   <div class="container ">
   <div class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
       
-        <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
 
           {{-- link bootstrap.js to action  --}}
           <script src="{{ asset('public/assets/js/bootstrap.min.js') }}" ></script>
         {{-- link style.css to disegn header tamplate --}}
-        <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/style.min.css') }}"rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
           <p class="col-md-4 mb-0 text-muted">&copy; جميع الحقوق محفوظه {{date('Y')}}</p>
       <a href="{{ route('home') }}"
               class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        {{-- <img id="bot"src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png"width="40"
-                  height="32" alt=""/> --}}
+        <img id="bot"src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png"width="40"
+                  height="32" alt=""/>
       </a>
       <ul class="nav col-md-4 justify-content-end">
           <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">الرئيسية</a></li>
--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -8,27 +8,9 @@
     <meta name="author" content="@yield('author')">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="generator" content="Hugo 0.101.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 </head>
-
-
-<header>
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    {{-- style bootstrap --}}
-    {{-- Bootstrap  v5.2.2 (https://getbootstrap.com/) --}}
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>  --}}
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> --}}
-    {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
-
-    <!-- include summernote css/js -->
-    {{-- <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" /> --}}
-    {{-- Vue fremwork cdn --}}
-    {{-- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> --}}
-
-</header>
-
 
 <nav id="heading" class="navbar bg-body-tertiary fixed-top ">
     <div class="container-fluid">
