593be34ea4ba1f898cce3b932f3457f3cdf3850a
Alawi8 <alialshalawi9@gmail.com>
2024-01-04 07:23:37 +0300

    improve performance

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -11,7 +11,7 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/bitmap.png') }}">
     <meta name="generator" content="Hugo 0.101.0">
     {{-- Bootstrap link --}}
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-VNPVF4DM3C"></script>
