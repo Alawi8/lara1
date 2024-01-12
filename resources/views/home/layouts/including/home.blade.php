f16d9c8e7247df0e80408b8e156df3bdfc8692e1
Alawi8 <alialshalawi9@gmail.com>
2024-01-12 03:22:27 +0300

    improve performance

--- resources/views/home/layouts/including/home.blade.php
+++ resources/views/home/layouts/including/home.blade.php
@@ -40,10 +40,10 @@
             <div class="row">
                 @if (isset($categories))
                     @foreach ($categories as $category)
-                        <div class="col-md-4 mb-4">
+                        <div id="" class="col-md-4 mb-4 card ">
                             <a href="{{ route('categories.show', $category->id) }}" class="nav-link">
                             <!-- Jumbotron -->
-                            <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white">
+                            <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 ">
                                 <h1 class="mb-3 h2">{{ $category->name }}</h1>
                                 <p>
                                     {{ Str::limit($category->title, 110) }}
