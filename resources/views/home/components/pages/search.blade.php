ffe29e49843471f2a25490c730ffda17d66cf162
Alawi8 <alialshalawi9@gmail.com>
2024-01-14 11:44:21 +0300

    improve system

--- resources/views/home/components/pages/search.blade.php
+++ resources/views/home/components/pages/search.blade.php
@@ -10,23 +10,21 @@
         </div>
     @else
         @forelse($result as $article)
-            <a href="#" class="col-md-6 nav-link">
+            <a href="{{ route('display', $article->id) }}" class="col-md-6 nav-link">
                 <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                     <div class="col p-4 d-flex flex-column position-static">
                         <strong class="d-inline-block mb-2 text-primary">World</strong>
                         <h3 class="mb-0">{{ $article->title }}</h3>
-                        <div class="mb-1 text-muted">Nov 12</div>
-                        <p class="card-text mb-auto">{{ $article->excerpt }}</p>
+                        <div class="mb-1 text-muted">{{ \Carbon\Carbon::parse($article->date)->diffForHumans() }}</div>
+                        <p class="card-text mb-auto">{{ $article->exept}}</p>
                         {{-- Continue reading</a> --}}
                     </div>
                     <div class="col-auto d-none d-lg-block">
-                        <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg"
-                            role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
-                            focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef"
-                                dy=".3em">Thumbnail</text>
-                        </svg>
+                        <svg class="bd-placeholder-img" width="200" height="250" xmlns=""
+                        role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
+                        focusable="false" style="background-image: url('{{$article->image_path}}'); background-size: cover; background-repeat: no-repeat;">
+                    </svg>
+                    
                     </div>
                 </div>
             </a>
