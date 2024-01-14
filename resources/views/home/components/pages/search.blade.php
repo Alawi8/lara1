fdbc473f8ac9ebe76454bcc7afb671efe20e8598
Alawi8 <alialshalawi9@gmail.com>
2024-01-14 12:34:07 +0300

    improve sestem

--- resources/views/home/components/pages/search.blade.php
+++ resources/views/home/components/pages/search.blade.php
@@ -13,7 +13,7 @@
             <a href="{{ route('display', $article->id) }}" class="col-md-6 nav-link">
                 <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                     <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary">World</strong>
+                        <strong class="d-inline-block mb-2 text-primary">النتيجه</strong>
                         <h3 class="mb-0">{{ $article->title }}</h3>
                         <div class="mb-1 text-muted">{{ \Carbon\Carbon::parse($article->date)->diffForHumans() }}</div>
                         <p class="card-text mb-auto">{{ Str::limit($article->exept, 150) }}</p>
