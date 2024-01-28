cc6eb1fdb85b69e410215819610747d2b3d95d82
Alawi8 <alialshalawi9@gmail.com>
2024-01-28 10:37:14 +0300

    create livewire

--- resources/views/livewire/search-form.blade.php
+++ resources/views/livewire/search-form.blade.php
@@ -1,16 +1,16 @@
 <div>
-    <form>
-        <input class="form-control" wire:model="searchTerm" type="text" placeholder="عن ماذا تبحث">
+    <form  method="GET">
+        @csrf
+        <input class="form-control" wire:model="searchTerm" type="text" placeholder="عن ماذا تبحث"  >
     </form>
 
     <ul class="list-group">
         @if($result && $result->count() > 0)
             @foreach($result as $post)
-                <a href="#" class="list-group-item">{{$post->title}}</a>
-
+                <a href="{{ route('display', ['title' => str_replace(' ','_',$post->title)]) }}" class="list-group-item">{{$post->title}}</a>
                 
             @endforeach
         @else
         @endif
-    </ul>
+    </ul>ؤؤ
 </div>
