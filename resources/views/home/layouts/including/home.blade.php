1b46455e6c00a1debdb1474b9b272ea501c6f3a1
Alawi8 <alialshalawi9@gmail.com>
2024-02-24 18:19:01 +0300

    fix some errors

--- resources/views/home/layouts/including/home.blade.php
+++ resources/views/home/layouts/including/home.blade.php
@@ -1,9 +1,12 @@
 <div class="container-fluid">
 
-
+    <video class="img-fluid" autoplay loop muted controls  height="300" volume="0.5">
+        <source src='{{ asset('Video/موسيقى يوم التأسيس _ Saudia foundingday music.mp4') }}' type="video/mp4" />
+    </video>
+    
+    <!-- Carousel wrapper -->
     @if (isset($all_posts))
         <div class="row">
-
 
             @forelse ($all_posts as $post)
                 <div class="col-xxl-3 col-xl-4 col-lg-4 col-md-6 col-sm-12 ">
