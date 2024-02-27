4e1dd8fe5c82414f3df7a634adcc3cc45d9852c0
Alawi8 <alialshalawi9@gmail.com>
2024-02-27 06:39:00 +0300

    update schema

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -112,7 +112,7 @@ class HomeController extends Controller
         $this->seo()->addImages([
             [
                 'url' => $post->img_url,
-                'type' => 'image/jpeg',
+                'type' => 'ImageObject',
                 'name' => $post->title,
             ]
 
