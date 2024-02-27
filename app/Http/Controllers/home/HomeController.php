4d3561b933368e411da679c7282274cbc5f71df2
Alawi8 <alialshalawi9@gmail.com>
2024-02-27 06:32:17 +0300

    update

--- app/Http/Controllers/home/HomeController.php
+++ app/Http/Controllers/home/HomeController.php
@@ -10,7 +10,7 @@ use App\Models\Post;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Illuminate\Support\Str;
 use Artesaos\SEOTools\Traits\SEOTools as SEOToolsTrait;
-use Artesaos\SEOTools\Traits\JsonLdMulti ;
+use Artesaos\SEOTools\Traits\JsonLdMulti;
 use Illuminate\Support\Facades\View;
 use Artesaos\SEOTools\Facades\TwitterCard as TwitterCardTrait;
 
@@ -21,6 +21,9 @@ use Artesaos\SEOTools\Facades\JsonLd;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Spatie\SchemaOrg\LocalBusiness;
 use Spatie\SchemaOrg\Schema;
+use Carbon\Carbon;
+
+
 class HomeController extends Controller
 {
     use SEOToolsTrait;
@@ -37,19 +40,13 @@ class HomeController extends Controller
         $this->seo()->twitter()->setSite('@alo0o0o01');
         $this->seo()->jsonLd()->setType('WebPage');
 
-        $business = ['name' => 'Spatie'];
 
-$localBusiness = Schema::localBusiness()
-    ->name($business['name'])
-    ->if(isset($business['email']), function (LocalBusiness $schema) use ($business) {
-        $schema->email($business['email']);
-    });
         // category methods for articles 
 
         $categories = Category::select('title', 'name', 'id')->paginate(6);
 
         # select posts 
-        $all_posts = Post::select('title', 'img_url', 'id' ,'created_at')
+        $all_posts = Post::select('title', 'img_url', 'id', 'created_at')
             ->orderBy('created_at', 'desc')
             ->paginate(12);
 
@@ -77,7 +74,7 @@ $localBusiness = Schema::localBusiness()
         # get results from Post model
         $result = Post::where('title', 'like', '%' . $q . '%')
             ->orWhere('slug', 'like', '%' . $q . '%')
-            ->select('title', 'slug' , 'img_url') // اختر الحقول التي تحتاجها هنا
+            ->select('title', 'slug', 'img_url') // اختر الحقول التي تحتاجها هنا
             ->paginate(8);
 
         return view('home.components.pages.search', compact('result'));
@@ -112,22 +109,48 @@ $localBusiness = Schema::localBusiness()
         $post = Post::where('title', $title)->firstOrFail();
         $this->seo()->setTitle("{$post->title}");
         $this->seo()->setDescription($post->slug);
+        $this->seo()->addImages([
+            [
+                'url' => $post->img_url,
+                'type' => 'image/webp',
+                'name' => $post->title,
+            ]
+
+        ]);
+        //opengraph
+        // article
+
         $this->seo()->opengraph()->setUrl($post->slug);
         $this->seo()->opengraph()->addProperty('type', 'articles');
-        $this->seo()->addImages($post->img_url);
-        $this->seo()->setCanonical(route('display', ['title' => str_replace(' ','_',$post->title)]) );
+
+        $this->seo()->setCanonical(route('display', ['title' => str_replace(' ', '_', $post->title)]));
         $this->seo()->jsonLd()->setType('Article');
 
         # CREATE SEO SERVICES 
+        $published_time = Carbon::parse($post->created_at)->format('Y-m-d\TH:i:sP');
+        $modified_time = Carbon::parse($post->updated_at)->format('Y-m-d\TH:i:sP');
+
+        $this->seo()->opengraph()
+            ->setTitle($post->title)
+            ->setDescription($post->slug)
+            ->setType('article')
+            ->setArticle([
+                'published_time' => $published_time,
+                'modified_time' => $modified_time,
+                'author' => ['name' => 'meshcah'],
+            ])
+            ->setUrl($post->slug);
+
         $this->seo()->jsonLd()
             ->setType('Article')
             ->setTitle($post->title)
             ->setDescription($post->slug)
-            ->addImage(asset($post->img_url));
+            ->setUrl(route('display', ['title' => str_replace(' ', '_', $post->title)]));
+
         $this->seo()->twitter()
             ->setTitle($post->title)
             ->setDescription($post->slug)
-            ->setImage($post->img_url)
+            ->setImage(asset($post->img_url))
             ->setSite('meshcah');
 
         # GET COMMENT BY TITLE 
