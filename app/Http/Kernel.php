c7e3060c3855606a15e3110a2f61ff8cc64bbc4d
Alawi8 <alialshalawi9@gmail.com>
2024-05-27 07:38:32 +0300

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'csp'      =>\App\Http\Middleware\CspMiddleware::class,
-        
+               
 
     ];
 }
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -9,7 +9,7 @@ import axios from 'axios';
 
 export default {
   mounted(){
-    axios.get('https://testing.meshcah.net/relation')
+    axios.get('https://testing.meshcah.net/public/relation')
     .then(response => console.log(response));
   }
 };
