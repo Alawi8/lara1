3b7cec399b605c3e93fe4a2c839790d4209b8a32
Alawi8 <alialshalawi9@gmail.com>
2024-05-27 07:27:06 +0300

    update cros

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,48 +1,16 @@
 <template>
+  <div>
+    
+  </div>
+</template>
 
-      <h1>قائمة المقالات</h1>
-      <div v-if="errorMessage" class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>خطأ!</strong> {{ errorMessage }}
-        <button type="button" class="close" @click="closeAlert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <ul>
-        <li v-for="post in posts" :key="posts.id">
-          <h2>{{ post.title }}</h2>
-          <div v-html="post.excerpt.rendered"></div>
-        </li>
-      </ul>
+<script>
+import axios from 'axios';
 
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        posts: [],
-        errorMessage: ''
-      };
-    },
-    created() {
-      this.fetchPosts();
-    },
-    methods: {
-      async fetchPosts() {
-        try {
-          const d = await axios.get('https://meshcah.net/relation');
-            console.log(d);
-        } catch (error) {
-          console.error('Error fetching posts:', error);
-          this.errorMessage = 'حدث خطأ أثناء جلب المقالات. يرجى المحاولة مرة أخرى لاحقًا.';
-        }
-      },
-      closeAlert() {
-        this.errorMessage = '';
-      }
-    }
+export default {
+  mounted(){
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => console.log(response));
   }
-  </script>
-  
+};
+</script>
